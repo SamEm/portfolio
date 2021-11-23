3869ff8e75c317fa38600cc9c4cac6e1b820ae56
Logiz <l.samemami@gmail.com>
2021-11-23 12:30:41 +0100

    Temp removed for performance reasons

--- src/utils/mousePosition.tsx
+++ src/utils/mousePosition.tsx
@@ -3,13 +3,19 @@ import {useEffect, useState } from 'react';
 export const MousePosition = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  useEffect(() => {
-    const setEvent = (e:any) => setPosition({x: e.clientX, y: e.clientY });
-    window.addEventListener('mousemove', setEvent);
-    return () => {
-      window.removeEventListener('mousemove', setEvent);
-    }
-  }, [])
+  // useEffect(() => {
+  //   const windowWidth = window.innerWidth / 1;
+  //   const windowHeight = window.innerHeight / 1;
+  //   const setEvent = (e: any) =>
+  //     setPosition({
+  //       x: e.clientX / windowWidth,
+  //       y: e.clientY / windowHeight,
+  //     });
+  //   window.addEventListener("mousemove", setEvent);
+  //   return () => {
+  //     window.removeEventListener("mousemove", setEvent);
+  //   };
+  // }, [])
 
   return position;
 }
